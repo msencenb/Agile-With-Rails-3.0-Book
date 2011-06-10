99c9c48adcc2d42fbfc647679c17a0ff0ce0426a
Matt Sencenbaugh <matt_sencenbaugh@intuit.com>
2011-06-10 16:12:31 -0700

    On page 123

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,18 @@ validates :image_url, :format => {
 	:message => 'must be a URL for GIF, JPG or PNG image.'
 	}
 default_scope :order => 'title'
+has_many :line_items
+before_destroy :ensure_not_referenced_by_any_line_item
+
+
+private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
 end
